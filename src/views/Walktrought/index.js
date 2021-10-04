d69b498664efed64147a512e54a965dba1248147
Louis Capelle <louis.capelle@epitech.eu>
2021-10-04 12:49:20 +0200

    [ADD] Added tree structure, beggining work of waltrought

---
+++ src/views/Walktrought/index.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+export default WalkTrought = () => {
+    return (
+        <View>
+            <Text>Walktrought</Text>
+        </View>
+    )
+}

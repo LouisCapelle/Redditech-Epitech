a83dc08ad4f4601c11e72140c1df694490e0185c
Louis Capelle <louis.capelle@epitech.eu>
2021-10-04 16:32:29 +0200

    [ADD] Added animation and walktrought on startup, let's maxime clean the code

--- src/views/Walktrought/index.js
+++ src/views/Walktrought/index.js
@@ -1,10 +1,218 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Dimensions, Button, Touchable, TouchableOpacity, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, withDelay } from 'react-native-reanimated';
+import LottieView from 'lottie-react-native';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+let pages = width * 2;
 
 export default WalkTrought = () => {
+    const offset = useSharedValue(0);
+    const offsetLogo = useSharedValue(0);
+    const offsetText = useSharedValue(0);
+    const offsetConnection = useSharedValue(width);
+    const offsetConnectionLogo = useSharedValue(width);
+    const offsetConnectionButton = useSharedValue(width);
+    const [page, setPage] = useState(0);
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offset.value }],
+        };
+    });
+
+    const animatedStylesLogo = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offsetLogo.value }],
+        };
+    });
+
+    const animatedStylesText = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offsetText.value }],
+        };
+    });
+
+    const animatedStylesConnection = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offsetConnection.value }],
+        };
+    });
+
+    const animatedStylesConnectionLogo = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offsetConnectionLogo.value }],
+        };
+    });
+
+    const animatedStylesConnectionButton = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offsetConnectionButton.value }],
+        };
+    });
+
     return (
-        <View>
-            <Text>Walktrought</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Animated.View style={[animatedStyles, styles.background]}>
+                <LinearGradient
+                    colors={['rgba(106, 247, 135, 0.7)', 'transparent']}
+                    style={styles.background}
+                    start={{ x: 0, y: 1 }}
+                    end={{ x: 2, y: 1 }}
+                />
+            </Animated.View>
+            <Animated.View style={[animatedStylesLogo, {marginTop: 50}]}>
+                <LottieView
+                    ref={animation => {
+                        this.animation = animation;
+                    }}
+                    style={{
+                        width: 100,
+                        height: 200,
+                    }}
+                    source={require('../../../assets/reddit.json')}
+                    autoPlay
+                />
+            </Animated.View>
+            <Animated.View style={[animatedStylesText]}>
+                <View style={{justifyContent:'center', alignItems: 'center'}}>
+                    <Text style={{fontSize: 25, fontWeight: '400'}}> Bienvenue sur ReddiTech </Text>
+                    <Text style={{textAlign: 'justify', width: width - 70, marginTop: 15, fontWeight: '300', fontSize: 15}}>
+                        Cette application vous permet de trouver des articles de reddit en fonction de vos préférences.
+                        C'est un projet Epitech du module AppDev qui a été réalisé par des étudiants de l'école.
+                    </Text>
+                </View>
+            </Animated.View>
+            <Animated.View style={[animatedStylesConnectionLogo, {position: 'absolute', top: 150}]}>
+                <LottieView
+                    ref={animation => {
+                        this.animation = animation;
+                    }}
+                    style={{
+                        width: 100,
+                        height: 100,
+                    }}
+                    source={require('../../../assets/connection.json')}
+                    autoPlay
+                />
+            </Animated.View>
+            <Animated.View style={[animatedStylesConnection, {position: 'absolute', top: 250}]}>
+                <View style={{justifyContent:'center', alignItems: 'center'}}>
+                    <Text style={{fontSize: 25, fontWeight: '400'}}> Connexion </Text>
+                    <Text style={{textAlign: 'justify', width: width - 70, marginTop: 15, fontWeight: '400', fontSize: 15}}>
+                        Pour accéder à notre application. Vous allez devoir autoriser nos services lorsque cela vous sera demandé.
+                        Notre application a besoin de se connecter aux services de reddit afin de pouvoir récupérer les informations.
+                    </Text>
+                </View>
+            </Animated.View>
+            <Animated.View style={[animatedStylesConnectionButton, {position: 'absolute', top: 430, alignItems: 'center', width: width}]}>
+                <TouchableOpacity style={{
+                    backgroundColor: 'white', width: '80%', height: 40, borderRadius: 10, justifyContent: 'center', alignItems: 'center',
+                    shadowColor: 'rgba(0, 0, 0, 0.1)',
+                    shadowOpacity: 0.8,
+                    elevation: 1,
+                    shadowRadius: 15 ,
+                    shadowOffset : { width: 1, height: 13 },
+                    flexDirection: 'row',
+                }} activeOpacity={.7}>
+                    <Image source={require('../../../assets/reddit_button.png')} style={{width: 20, height: 20}}/>
+                    <Text style={{fontWeight: '500', fontSize: 17, marginLeft: 5}}>
+                        Se connecter avec Reddit
+                    </Text>
+                </TouchableOpacity>
+            </Animated.View>
+            <View style={styles.bottomView}>
+                <TouchableOpacity style={{
+                    backgroundColor: 'white', width: '80%', height: 40, borderRadius: 10, justifyContent: 'center', alignItems: 'center',
+                    shadowColor: 'rgba(0, 0, 0, 0.1)',
+                    shadowOpacity: 0.8,
+                    elevation: 1,
+                    shadowRadius: 15 ,
+                    shadowOffset : { width: 1, height: 13},
+                }} onPress={() => {
+                    if ( page < 1 ) {
+                        offset.value = withSpring(offset.value - width, {
+                            damping: 20,
+                            stiffness: 90,
+                        })
+                        offsetLogo.value = withDelay(200, withSpring(offsetLogo.value - width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetText.value = withDelay(250, withSpring(offsetText.value - width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetConnection.value = withDelay(300, withSpring(offsetConnection.value - width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetConnectionLogo.value = withDelay(350, withSpring(offsetConnectionLogo.value - width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetConnectionButton.value = withDelay(300, withSpring(offsetConnectionButton.value - width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        setPage(page + 1);
+                    } else {
+                        offset.value = withSpring(offset.value + width, {
+                            damping: 20,
+                            stiffness: 90,
+                        })
+                        offsetLogo.value = withDelay(200, withSpring(offsetLogo.value + width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetText.value = withDelay(250, withSpring(offsetText.value + width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetConnection.value = withDelay(300, withSpring(offsetConnection.value + width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetConnectionLogo.value = withDelay(350, withSpring(offsetConnectionLogo.value + width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        offsetConnectionButton.value = withDelay(400, withSpring(offsetConnectionButton.value + width, {
+                            damping: 20,
+                            stiffness: 90,
+                        }));
+                        setPage(page - 1);
+                    }
+                }} activeOpacity={.7}>
+                    <Text style={{fontWeight: '500', fontSize: 17}}>
+                        {(page === 1) ? 'Précédent' : 'Suivant'}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        width: pages,
+        height: height,
+    },
+    bottomView: {
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1,
+        width: '100%',
+    }
+});

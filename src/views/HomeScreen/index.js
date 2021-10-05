017632b052dd1d8a7232436bced6d4ee2bc28246
Louis Capelle <louis.capelle@epitech.eu>
2021-10-05 15:49:38 +0200

    [ADD] Added TextField components

--- src/views/HomeScreen/index.js
+++ src/views/HomeScreen/index.js
@@ -7,8 +7,8 @@ export default HomeScreen = () => {
     const appContext = useContext(AppContext);
 
     return (
-        <SafeAreaView style={{backgroundColor: appContext.darkMode ? "grey" : "white"}}>
-            <TextField text={"HomeScreen"}/>
+        <SafeAreaView style={{backgroundColor: appContext.darkMode ? "grey" : "white", height: '100%', width: '100%', alignItems: 'center'}}>
+            <TextField iconName="search1" iconSize={18} iconColor="black" placeHolder="Rechercher" />
         </SafeAreaView>
     );
 }
